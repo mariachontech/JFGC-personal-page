ae1bbe9869c894512491ead3fa7a5c05c4c4df87
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-05-07 13:04:31 -0600

    building bio_07

--- src/app/(pages)/research/_components/citation-pub.js
+++ src/app/(pages)/research/_components/citation-pub.js
@@ -37,7 +37,7 @@ export function CitationPub({ doi }) {
       description: (
         <p
           dangerouslySetInnerHTML={{ __html: refs2.bibtex }}
-          className="bg-slate-700 text-white  p-2"
+          className="bg-amber-300 text-black  p-2"
         />
       ),
     })
