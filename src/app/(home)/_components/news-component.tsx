842a55b1034fa45d14e1c98213680c039c9a9a28
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-07-15 11:42:25 -0600

    news detail

--- src/app/(home)/_components/news-component.tsx
+++ src/app/(home)/_components/news-component.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { News } from '../../../../types'
 import { urlForImage } from '../../../../sanity/lib/image'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 type Props = {
   news: News[]
@@ -27,7 +28,7 @@ export const NewsComponent = ({ news }: Props) => {
   console.log('message', news)
 
   return (
-    <div className="max-w-screen-xl  mx-auto py-10">
+    <div className="w-[90%] md:max-w-screen-xl  mx-auto py-10">
       <h3 className="font-bold py-5">News</h3>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-0 lg:grid-rows-2">
@@ -39,13 +40,19 @@ export const NewsComponent = ({ news }: Props) => {
               className={cn(
                 'relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96',
                 ind === 0
-                  ? 'md:col-span-2 lg:row-span-2 lg:h-full group dark:bg-gray-500'
-                  : 'group dark:bg-gray-500',
+                  ? 'md:col-span-2 lg:row-span-2 lg:h-full group dark:bg-gray-500/20'
+                  : 'group dark:bg-gray-500/20',
               )}
-              style={{
-                backgroundImage: `url(${urlForImage(newData.imageOfEvent.asset as any)})`,
-              }}
+              // style={{
+              //   backgroundImage: `url(${urlForImage(newData.imageOfEvent.asset as any)})`,
+              // }}
             >
+              <Image
+                src={urlForImage(newData.imageOfEvent.asset as any)}
+                alt={newData.imageOfEvent.alt}
+                fill
+                className="object-contain"
+              />
               <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
               <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
                 <a
