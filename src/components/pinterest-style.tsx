05bcba531f0aafdf7a577d15e681d29c81cae390
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-08-25 10:59:11 -0600

    Gallery FErmi and modal

--- src/components/pinterest-style.tsx
+++ src/components/pinterest-style.tsx
@@ -5,6 +5,16 @@ import { Category, Gallery2 } from '../../types'
 import { urlForImage } from '../../sanity/lib/image'
 import { Badge } from './ui/badge'
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
 type Props = {
   pics: Gallery2[] | []
   categories: Category[] | []
@@ -16,37 +26,68 @@ const PinterestStyle = ({ pics, categories }: Props) => {
       {pics?.map((pic, index) => (
         <div
           key={index}
-          className="  relative  group  cursor-pointer overflow-hidden rounded-md"
+          className="relative  group  cursor-pointer overflow-hidden rounded-md"
         >
-          <Image
-            src={urlForImage(pic?.imageGallery as any)}
-            alt=""
-            style={{
-              width: '100%',
-              height: 'auto',
-            }}
-            width={500}
-            height={300}
-            // style={{ width: 'auto', height: 'auto' }}
-          />
-          <div className="absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100">
-            <div className="pointer-events-none absolute inset-0 bg-black opacity-30"></div>
-            <div className="absolute inset-0 flex flex-col p-8">
-              {/* <button className="mr-auto rounded-full bg-red-500 py-2 px-8 text-sm font-bold text-white">
+          <Dialog>
+            <DialogTrigger>
+              <Image
+                src={urlForImage(pic?.imageGallery as any)}
+                alt=""
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                }}
+                width={500}
+                height={300}
+                // style={{ width: 'auto', height: 'auto' }}
+              />
+              <div className="absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100">
+                <div className="pointer-events-none absolute inset-0 bg-black opacity-30"></div>
+                <div className="absolute inset-0 flex flex-col p-8">
+                  {/* <button className="mr-auto rounded-full bg-red-500 py-2 px-8 text-sm font-bold text-white">
                 Share
               </button> */}
-              <div className="flex  justify-start items-center gap-1">
-                {categories.map((cat) => (
-                  <Badge key={cat._id} variant={'destructive'}>
-                    {cat.title}
-                  </Badge>
-                ))}
+                  <div className="flex  justify-start items-center gap-1">
+                    {categories.map((cat) => (
+                      <Badge key={cat._id} variant={'destructive'}>
+                        {cat.title}
+                      </Badge>
+                    ))}
+                  </div>
+                  <div className="relative z-10 mt-auto break-words text-lg font-semibold text-white">
+                    {pic?.title}
+                  </div>
+                </div>
               </div>
-              <div className="relative z-10 mt-auto break-words text-lg font-semibold text-white">
-                {pic?.title}
-              </div>
-            </div>
-          </div>
+            </DialogTrigger>
+            <DialogContent className="bg-transparent">
+              <DialogHeader>
+                <DialogTitle className="w-full text-center py-2">
+                  {pic.title}
+                </DialogTitle>
+                <Image
+                  src={urlForImage(pic?.imageGallery as any)}
+                  alt=""
+                  style={{
+                    width: '100%',
+                    height: 'auto',
+                  }}
+                  width={500}
+                  height={300}
+                  // style={{ width: 'auto', height: 'auto' }}
+                />
+              </DialogHeader>
+              <DialogFooter>
+                <div className="flex  justify-start items-center gap-1">
+                  {categories.map((cat) => (
+                    <Badge key={cat._id} variant={'destructive'}>
+                      {cat.title}
+                    </Badge>
+                  ))}
+                </div>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       ))}
     </div>
