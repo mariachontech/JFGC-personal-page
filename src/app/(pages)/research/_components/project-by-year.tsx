6d7436367926cfb8c2dd6c871b55afa45e9705f6
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2025-05-03 17:38:05 -0600

    projects corrections 3

--- src/app/(pages)/research/_components/project-by-year.tsx
+++ src/app/(pages)/research/_components/project-by-year.tsx
@@ -20,7 +20,7 @@ export const ProjectByYear = ({ projects }: Props) => {
       <div className="container px-4 py-16 mx-auto space-y-8 lg:max-w-3xl">
         <h2 className="text-2xl font-bold md:text-4xl">Timeline</h2>
         <div className="space-y-8">
-          {years.map((year) => (
+          {years?.map((year) => (
             <div key={year}>
               <h3 className="mb-3 text-lg font-bold md:text-xl">{year}</h3>
               <ul className="space-y-4">
@@ -35,24 +35,24 @@ export const ProjectByYear = ({ projects }: Props) => {
                         <path d="M426.072,86.928A238.75,238.75,0,0,0,88.428,424.572,238.75,238.75,0,0,0,426.072,86.928ZM257.25,462.5c-114,0-206.75-92.748-206.75-206.75S143.248,49,257.25,49,464,141.748,464,255.75,371.252,462.5,257.25,462.5Z"></path>
                         <polygon points="221.27 305.808 147.857 232.396 125.23 255.023 221.27 351.063 388.77 183.564 366.142 160.937 221.27 305.808"></polygon>
                       </svg>
-                      <h4 className="font-medium">{project.title}</h4>
+                      <h4 className="font-medium">{project?.title}</h4>
                     </div>
-                    {project.description && (
+                    {project?.description && (
                       <p className="ml-7 dark:text-gray-400">
-                        {project.description}
+                        {project?.description}
                       </p>
                     )}
-                    {project.id_key && (
+                    {project?.id_key && (
                       <p className="ml-7 dark:text-gray-400">
-                        {project.id_key}
+                        {project?.id_key}
                       </p>
                     )}
-                    {project.type && (
-                      <p className="ml-7 dark:text-gray-400">{project.type}</p>
+                    {project?.type && (
+                      <p className="ml-7 dark:text-gray-400">{project?.type}</p>
                     )}
-                    {project.finantial && (
+                    {project?.finantial && (
                       <p className="ml-7 dark:text-gray-400">
-                        {project.finantial}
+                        {project?.finantial}
                       </p>
                     )}
                   </li>
